ce27b2f03927d3cf3793116fafd5b1a1891adaf9
Arindam Das <dasarindam.mails@gmail.com>
2024-06-28 10:59:30 +0530

    feat: outline memcached headers and constants

--- memcached-ebpf-proxy-cache-common/src/lib.rs
+++ memcached-ebpf-proxy-cache-common/src/lib.rs
@@ -1,1 +1,25 @@
 #![no_std]
+
+use aya_ebpf::bindings::bpf_spin_lock;
+
+pub const MAX_KEY_LENGTH: usize = 250;
+pub const MAX_VAL_LENGTH: usize = 1000;
+pub const MAX_ADDITIONAL_PAYLOAD_BYTES: usize = 53;
+pub const MAX_CACHE_DATA_SIZE: usize =
+    MAX_KEY_LENGTH + MAX_VAL_LENGTH + MAX_ADDITIONAL_PAYLOAD_BYTES;
+
+pub const MAX_KEY_IN_MULTIGET: usize = 30;
+pub const MAX_CACHE_ENTRY_COUNT: usize = 3250000;
+
+pub const MAX_PACKET_LENGTH: usize = 1500;
+pub const MAX_KEY_IN_PACKET: usize = MAX_KEY_IN_MULTIGET;
+
+pub const FNV_OFFSET_BASIS_32: u32 = 2166136261;
+pub const FNV_PRIME_32: u32 = 16777619;
+
+pub struct CacheEntry {
+    pub lock: bpf_spin_lock,
+    pub len: u32,
+    pub valid: u8,
+    pub hash: u32,
+}
--- memcached-ebpf-proxy-cache-ebpf/src/main.rs
+++ memcached-ebpf-proxy-cache-ebpf/src/main.rs
@@ -4,6 +4,14 @@
 use aya_ebpf::{bindings::xdp_action, macros::xdp, programs::XdpContext};
 use aya_log_ebpf::info;
 
+#[repr(C)]
+pub struct MemcachedUdpHeader {
+    pub request_id: u16,
+    pub seq_num: u16,
+    pub num_dgram: u16,
+    pub unused: u16,
+}
+
 #[xdp]
 pub fn memcached_ebpf_proxy_cache(ctx: XdpContext) -> u32 {
     match try_memcached_ebpf_proxy_cache(ctx) {

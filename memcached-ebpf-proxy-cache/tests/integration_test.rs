44826f09d7868c58d3eb3a8c7dc9a3cc0147b15d
Arindam Das <dasarindam.mails@gmail.com>
2024-09-19 15:57:30 +0530

    feat: correct test

--- memcached-ebpf-proxy-cache/tests/integration_test.rs
+++ memcached-ebpf-proxy-cache/tests/integration_test.rs
@@ -2,7 +2,7 @@ pub const MEMCACHED_GET_PROTOCOL: &str = "udp";
 
 pub const MEMCACHED_SET_PROTOCOL: &str = "tcp";
 
-pub const MEMCACHED_HOST: &str = "127.0.0.1";
+pub const MEMCACHED_HOST: &str = "0.0.0.0";
 
 pub const MEMCACHED_PORT: u16 = 11211;
 
@@ -28,5 +28,5 @@ fn memcache_get_set_consistency() {
     set_client.set(KEY, VAL, 10).unwrap();
 
     assert_eq!(get_client.get::<String>(KEY).unwrap().unwrap(), VAL);
-    assert_eq!(get_client.get::<String>(KEY).unwrap(), None);
+    assert_eq!(get_client.get::<String>(KEY).unwrap().unwrap(), VAL);
 }
